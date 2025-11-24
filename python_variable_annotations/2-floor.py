de8307d07c4687b158c476b24e378cf7fa9479c3
h7k2 <keddousheytem@gmail.com>
2025-11-24 15:10:21 +0100

    Add floor function to calculate the floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for getting the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+    """
+    return math.floor(n)
