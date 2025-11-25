35a720ee7e6f97e3ad85fc25c703a3dc85d99e5b
h7k2 <keddousheytem@gmail.com>
2025-11-25 15:03:38 +0100

    Add wait_n function for concurrent coroutine execution
    Implement wait_n function to execute multiple coroutines concurrently and return their delays.

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Execute multiple coroutines concurrently and return the delays
+in ascending order without using sort().
+"""
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays: List[float] = []
+
+    for completed in asyncio.as_completed(tasks):
+        delay = await completed
+        delays.append(delay)
+
+    return delays
