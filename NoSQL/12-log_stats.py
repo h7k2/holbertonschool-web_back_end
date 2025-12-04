2ffcc69f0c30c52114ef9f283b86850f04c713c6
h7k2 <keddousheytem@gmail.com>
2025-12-04 15:08:00 +0100

    refactor: Enhance code structure and readability in log_stats function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,15 +8,18 @@ def log_stats():
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
+    # Total number of logs
     total_logs = nginx_collection.count_documents({})
     print("{} logs".format(total_logs))
 
+    # Methods stats
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, count))
 
+    # Status check (GET /status)
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
