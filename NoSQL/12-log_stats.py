d091e12f99afd5b7a347a889df7cb6b50d7d4cda
h7k2 <keddousheytem@gmail.com>
2025-12-04 15:05:52 +0100

    refactor: Improve code readability and consistency in log_stats module

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python3
-"""Module providing statistics for Nginx logs stored in MongoDB.
-"""
-
+"""Script that provides stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
 
 def log_stats():
-    """Display basic statistics about Nginx logs."""
+    """Provides stats about Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+    nginx_collection = client.logs.nginx
 
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+    total_logs = nginx_collection.count_documents({})
+    print("{} logs".format(total_logs))
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        count = nginx_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
 
-    status_count = collection.count_documents(
+    status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-    print(f"{status_count} status check")
+    print("{} status check".format(status_check))
 
 
 if __name__ == "__main__":
